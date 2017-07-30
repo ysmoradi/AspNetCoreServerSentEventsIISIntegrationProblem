e382de3c54458f13b98d0aee0f40f4ae03607e51
ysmoradi <ysmoradi@outlook.com>
2017-07-30 12:54:27 +0430

    Add project files.

---
+++ AspNetCoreServerSentEventsIISIntegrationProblem/AppStartup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace AspNetCoreServerSentEventsIISIntegrationProblem
+{
+    public class AppStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDirectoryBrowser();
+        }
+
+        public void Configure(IApplicationBuilder aspNetCoreApp)
+        {
+            aspNetCoreApp.UseFileServer(new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true
+            });
+
+            aspNetCoreApp.Map("/sse", sseAspNetCoreApp =>
+            {
+                sseAspNetCoreApp.Run(async cntx =>
+                {
+                    cntx.Response.Headers.Add("Content-Type", "text/event-stream");
+
+                    for (var i = 0; true; ++i)
+                    {
+                        await cntx.Response.WriteAsync($"data: {i} at server side time: {DateTime.Now}\r\r", cntx.RequestAborted);
+
+                        await cntx.Response.Body.FlushAsync(cntx.RequestAborted);
+
+                        await Task.Delay(TimeSpan.FromSeconds(5));
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ AspNetCoreServerSentEventsIISIntegrationProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace AspNetCoreServerSentEventsIISIntegrationProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseWebRoot(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot"))
+                .UseStartup<AppStartup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
